5d38bfc0792aa2cb46b099a0accd916e27fa37c5
Nicolas Villalba <nerynicolas@gmail.com>
2018-11-18 07:12:47 -0300

    configuration class for static content served

---
+++ src/main/java/org/poc/imageservice/config/ResourceHandler.java
@@ -0,0 +1,18 @@
+package org.poc.imageservice.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ResourceHandler implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/image/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
