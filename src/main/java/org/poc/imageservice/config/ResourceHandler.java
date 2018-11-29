a815b29b18bd9cfe0f3b5439c9ac8aeca6bac3af
Nicolas Villalba <nerynicolas@gmail.com>
2018-11-29 04:16:14 -0300

    added config var to replace hardcoded file path

--- src/main/java/org/poc/imageservice/config/ResourceHandler.java
+++ src/main/java/org/poc/imageservice/config/ResourceHandler.java
@@ -1,6 +1,7 @@
 package org.poc.imageservice.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,10 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class ResourceHandler implements WebMvcConfigurer {
+
+    @Value("${images.resourse.location}")
+    private String location;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/image/**")
-                .addResourceLocations("file:/Users/Public/Documents/");
+                .addResourceLocations(this.location);
     }
 }
